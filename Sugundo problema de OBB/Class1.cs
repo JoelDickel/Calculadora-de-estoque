82cd741e6fa5060bcad298a2816a866491f3d218
JoelDickel <64815075+JoelDickel@users.noreply.github.com>
2023-01-09 12:27:56 -0300

    First Commit
    Subindo tudo

---
+++ Sugundo problema de OBB/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Sugundo_problema_de_OBB
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidades;
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidades;
+
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidades += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidadeR)
+        {
+            Quantidades -= quantidadeR;
+        }
+
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " + Quantidades + " unidades, Total: $ " 
+                + ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
